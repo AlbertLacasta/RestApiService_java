06f3197a59a53b83d6aa06fd9d9752bf52d2faa5
Albert Lacasta <47864341+AlbertLacasta@users.noreply.github.com>
2021-01-20 22:36:14 +0100

    added missing import statement

--- src/main/java/com/flashfind/flashfindapiservice/FlashFindApiServiceApplication.java
+++ src/main/java/com/flashfind/flashfindapiservice/FlashFindApiServiceApplication.java
@@ -1,5 +1,6 @@
 package com.flashfind.flashfindapiservice;
 
+import com.flashfind.flashfindapiservice.AuthFilters;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
